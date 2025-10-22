1d0b6fe117b66e2b6fe9ed5adfeb1424fbdaca73
Stephen Kinger <stephen.kinger@mappy.com>
2025-10-22 13:24:27 +0200

    set for a first test

---
+++ src/jira_id_hook.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+import re
+import os
+
+def get_current_branch_name():
+    result = subprocess.run(['git', 'symbolic-ref', '--short', 'HEAD'], capture_output=True, text=True)
+    if result.returncode != 0:
+        print("Error: Unable to get the current branch name.")
+        sys.exit(1)
+    return result.stdout.strip()
+
+def main(commit_msg_file):
+    branch_name = get_current_branch_name()
+
+    # Check for a pattern matching 'something_' after a '/'
+    if re.search(r'/.+-', branch_name):
+        # Extract the prefix from the branch name (everything after the first '/' and before the first '_')
+        match = re.search(r'.*/([[:alnum:]]+-[0-9]+)-.*', branch_name)
+        if match:
+            prefix = match.group(1)
+
+            # Check if the commit message file exists and is non-empty
+            if os.path.exists(commit_msg_file) and os.path.getsize(commit_msg_file) > 0:
+                with open(commit_msg_file, 'r+') as file:
+                    commit_msg = file.read().strip()
+
+                    if commit_msg.startswith(prefix):
+                        print("Prefix already set, it's good")
+                    elif commit_msg:
+                        # Prefix the commit message with the branch prefix
+                        file.seek(0)
+                        file.write(f"{prefix} {commit_msg}\n")
+                        file.truncate()
+                    else:
+                        print("Please set a comment for commit")
+                        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python add_branch_name.py <commit_msg_file>")
+        sys.exit(1)
+
+    main(sys.argv[1])
